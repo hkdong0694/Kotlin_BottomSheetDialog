81ad1037741a1794e487f5c0a73ab15a92f21119
한경동 <joel@youfriend.kr>
2021-06-10 22:53:30 +0900

    EditText 리스너 제어

--- app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.bottomsheetdialog
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.InputDevice
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), BottomSheetDialog.DialogListener {
@@ -30,9 +34,38 @@ class MainActivity : AppCompatActivity(), BottomSheetDialog.DialogListener {
             dialog?.dismiss()
         }
 
-        view.findViewById<EditText>(R.id.et_code).requestFocus()
+        var etText = view.findViewById<EditText>(R.id.et_code)
+        etText.requestFocus()
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(view.findViewById<EditText>(R.id.et_code), InputMethodManager.SHOW_IMPLICIT);
+        imm.showSoftInput(etText, InputMethodManager.SHOW_IMPLICIT)
+
+        etText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s?.length!! > 0) {
+                    view.findViewById<ImageView>(R.id.iv_et).visibility = View.VISIBLE
+                } else {
+                    view.findViewById<ImageView>(R.id.iv_et).visibility = View.GONE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+        
+        view.findViewById<ImageView>(R.id.iv_et).setOnClickListener {
+            etText.setText("")
+        }
+
+        view.findViewById<Button>(R.id.btn_identity).setOnClickListener {
+            view.findViewById<TextView>(R.id.tv_fail).visibility = View.VISIBLE
+            view.findViewById<View>(R.id.v_line).setBackgroundColor(Color.parseColor("#da1d1d"))
+        }
+
     }
 }
